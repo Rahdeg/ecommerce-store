87ee974f7b743f6eb78be61c9e9d9732427b6193
Rahdeg <walett95@gmail.com>
2023-07-17 17:20:21 +0100

    revalidation

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
 const getBillboard = async (id: string): Promise<Billboard> => {
-    const res = await fetch(`${URL}/${id}`, { next: { revalidate: 60 } });
+    const res = await fetch(`${URL}/${id}`, { next: { revalidate: 0 } });
     return res.json();
 
 }
