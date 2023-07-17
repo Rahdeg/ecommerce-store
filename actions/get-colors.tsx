de99d3e675875e8c79d234bf6156a2346326f0de
Rahdeg <walett95@gmail.com>
2023-07-17 17:36:44 +0100

    update revalidate

--- actions/get-colors.tsx
+++ actions/get-colors.tsx
@@ -3,7 +3,7 @@ import { Color } from "@/types"
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`;
 
 const getColors = async (): Promise<Color[]> => {
-    const res = await fetch(URL,{ next: { revalidate: 60 } });
+    const res = await fetch(URL,{ next: { revalidate: 0 } });
     return res.json();
 
 }
