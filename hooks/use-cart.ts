bb7d02bfb5f7e3dac3f1dbe57d113de7120df0bf
Rahdeg <walett95@gmail.com>
2023-07-21 17:09:34 +0100

    ss

--- hooks/use-cart.ts
+++ hooks/use-cart.ts
@@ -14,11 +14,11 @@ const useCart = create(
     persist<CratStoreProp>((set,get)=>({
         items:[],
         addItem: (data: Product)=>{
-            const currentItems = get().items;
-            const existingItem = currentItems.find((item)=> item.id === data.id);
-            if (existingItem) {
-                return toast("Item already in cart");
-            }
+            // const currentItems = get().items;
+            // const existingItem = currentItems.find((item)=> item.id === data.id);
+            // if (existingItem) {
+            //     return toast("Item already in cart");
+            // }
             set({items:[...get().items, data]});
             toast.success("Item added to cart.")
         },
